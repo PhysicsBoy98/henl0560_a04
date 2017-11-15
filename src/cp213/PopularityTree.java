651b9ed02a12926a4cf9adf425851327714250f0
zackvb <zackvb@hotmail.com>
2017-11-15 16:14:12 -0500

    fixed merge error

--- src/cp213/PopularityTree.java
+++ src/cp213/PopularityTree.java
@@ -10,68 +10,6 @@ package cp213;
  *            The data to store in the tree.
  */
 public class PopularityTree<T extends Comparable<T>> extends BST<T> {
-
-	@Override
-	public void leftLeftRotation(TreeNode<T> parent) {
-		TreeNode<T> root = parent.getLeft();
-		TreeNode<T> pivot = root.getLeft();
-		root.setLeft(pivot.getRight());
-		pivot.setRight(root);
-		parent.setLeft(pivot);
-	}
-
-	@Override
-	public void rightLeftRotation(TreeNode<T> parent) {
-		TreeNode<T> root = parent.getRight();
-		TreeNode<T> pivot = root.getLeft();
-		root.setLeft(pivot.getRight());
-		pivot.setRight(root);
-		parent.setRight(pivot);
-	}
-
-	@Override
-	public void leftRightRotation(TreeNode<T> parent) {
-		TreeNode<T> root = parent.getLeft();
-		TreeNode<T> pivot = root.getRight();
-		root.setRight(pivot.getLeft());
-		pivot.setLeft(root);
-		parent.setLeft(pivot);
-	}
-
-	@Override
-	public void rightRightRotation(TreeNode<T> parent) {
-		TreeNode<T> root = parent.getRight();
-		TreeNode<T> pivot = root.getRight();
-		root.setRight(pivot.getLeft());
-		pivot.setLeft(root);
-		parent.setRight(pivot);
-	}
-
-	public void leftRotation() {
-		TreeNode<T> root = this.root;
-		TreeNode<T> pivot = root.getLeft();
-		root.setLeft(pivot.getRight());
-		pivot.setRight(root);
-		this.root = pivot;
-	}
-
-	public void rightRotation() {
-		TreeNode<T> root = this.root;
-		TreeNode<T> pivot = root.getRight();
-		root.setRight(pivot.getLeft());
-		pivot.setLeft(root);
-		this.root = pivot;
-	}
-
-	// public void insert(final T data) {
-	// if (this.root == null) {
-	// this.root = new TreeNode<T>(data);
-	// this.size++;
-	// } else {
-	// insert_aux(data, root);
-	// }
-	// }
-
 	@Override
 	public void insert(final T data) {
 		if (this.root == null) {
