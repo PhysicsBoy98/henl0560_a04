e84dac4f6358e8a10387327215394ddb3a0d84bb
PhysicsBoy98 <33535222+PhysicsBoy98@users.noreply.github.com>
2017-11-14 21:40:53 -0500

    s

--- src/cp213/Main.java
+++ src/cp213/Main.java
@@ -9,16 +9,19 @@ public class Main {
 		System.out.println("Testing Insert");
 		BST<Integer> bst = new BST<Integer>();
 		bst.insert(9);
+		// DrawTree<Integer> draw = new DrawTree<Integer>(bst);
 		boolean bool = bst.contains(9);
 		System.out.println(bool);
-		bool = bst.valid();
+		// bool = bst.valid();
 		System.out.println(bool);
 		bst.insert(8);
 		bst.insert(10);
 		bst.insert(1);
 		bst.insert(9);
+		bool = bst.contains(1);
+		System.out.println(bool);
 
-		DrawTree<Integer> draw = new DrawTree<Integer>(bst);
+		DrawTree<Integer> draw2 = new DrawTree<Integer>(bst);
 
 	}
 
